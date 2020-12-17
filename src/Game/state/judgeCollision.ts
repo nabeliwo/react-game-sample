8c470665a162b397dca5b3881e3ea96900db179f
nabeliwo <hiroki.c.watanabe@gmail.com>
2020-12-17 20:41:32 +0900

    :sparkles: add collision between enemy and bullet

--- src/Game/state/judgeCollision.ts
+++ src/Game/state/judgeCollision.ts
@@ -18,74 +18,85 @@ const getIsSameArea = (aArea: Character['area'], bArea: Character['area']) => {
   return false
 }
 
-const getDistanceBetweenCharacter = (aPosition: Character['position'], bPosition: Character['position']) => {
+const getDistance = (aPosition: Character['position'], bPosition: Character['position']) => {
   const a = aPosition.x - bPosition.x
   const b = aPosition.y - bPosition.y
   return Math.sqrt(a * a + b * b)
 }
 
-const judgeEnemyCollisionWithPlayer = (
-  enemyPosition: Character['position'],
-  playerPosition: Character['position'],
-  onCollided: () => void,
-) => {
-  const distance = getDistanceBetweenCharacter(enemyPosition, playerPosition)
-
-  if (distance <= characterRadius + characterRadius) {
-    onCollided()
-  }
+const getIsCollide = (enemyPosition: Character['position'], playerPosition: Character['position']) => {
+  const distance = getDistance(enemyPosition, playerPosition)
+  return distance <= characterRadius + characterRadius
 }
 
-// const judgeCollisionWithBullet = (enemyCenter: Character['position'], BulletPosition: Character['position']) => {
-//   const bulletCenter = {
-//     x: BulletPosition.x + characterRadius,
-//     y: BulletPosition.y + characterRadius,
-//   }
-//   const distance = getDistanceBetweenCharacter(enemyCenter, bulletCenter)
-
-//   return distance <= characterRadius + characterRadius
-// }
-
 export const judgeCollision = (player: Player, bullets: Bullet[], enemies: Enemy[], gameOver: () => void) => {
+  const newEnemies: Enemy[] = []
+  const bulletMap = new Map<number, Bullet>()
+  const disappearedBulletIds: number[] = []
   const playerCenter = {
     x: player.position.x + characterRadius,
     y: player.position.y + characterRadius,
   }
-  // const newBullets: Bullet[] = []
-  const newEnemies: Enemy[] = []
+  const bulletGroups = bullets.reduce<{ [key: string]: Array<Bullet & { id: number }> }>((acc, bullet, i) => {
+    bulletMap.set(i, bullet)
+
+    for (const area of bullet.area) {
+      const bulletWithId = { id: i, ...bullet }
+
+      if (acc[area]) {
+        acc[area].push(bulletWithId)
+      } else {
+        acc[area] = [bulletWithId]
+      }
+    }
+
+    return acc
+  }, {})
 
   for (const enemy of enemies) {
+    let isCollided = false
     const enemyCenter = {
       x: enemy.position.x + characterRadius,
       y: enemy.position.y + characterRadius,
     }
 
     if (getIsSameArea(enemy.area, player.area)) {
-      judgeEnemyCollisionWithPlayer(enemyCenter, playerCenter, () => {
+      if (getIsCollide(enemyCenter, playerCenter)) {
         gameOver()
-      })
+        break
+      }
     }
 
-    newEnemies.push(enemy)
+    for (const area of enemy.area) {
+      const targetBullets = bulletGroups[area]
+
+      if (targetBullets && targetBullets.length > 0) {
+        for (const bullet of targetBullets) {
+          const bulletCenter = {
+            x: bullet.position.x + characterRadius,
+            y: bullet.position.y + characterRadius,
+          }
+
+          isCollided = getIsCollide(enemyCenter, bulletCenter)
+
+          if (isCollided) {
+            disappearedBulletIds.push(bullet.id)
+            break
+          }
+        }
+      }
+
+      if (isCollided) break
+    }
+
+    if (!isCollided) {
+      newEnemies.push(enemy)
+    }
   }
 
-  // enemies.map((enemy) => {
-  //   // let isCollided = false
+  for (const id of disappearedBulletIds) {
+    bulletMap.delete(id)
+  }
 
-  //   for (const _ of newBullets) {
-  //     // const _bulletCenter = {
-  //     //   x: bullet.position.x + characterRadius,
-  //     //   y: bullet.position.y + characterRadius,
-  //     // }
-  //     // isCollided = judgeCollisionWithBullet(enemyCenter, bullet.position)
-  //     // if (isCollided) break
-  //     // newBullets.push(bullet)
-  //   }
-
-  //   // if (isCollided) break
-
-  //   return enemy
-  // })
-
-  return { bullets: bullets, enemies: newEnemies }
+  return { bullets: Array.from(bulletMap.values()), enemies: newEnemies }
 }
